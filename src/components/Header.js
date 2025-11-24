6375d657a9902d1330f134cba1be8ffe2c8e95f2
Mateus Domingues Pasquantonio <99039953+mateuspasquantonio@users.noreply.github.com>
2025-11-24 06:57:36 -0600

    update the code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,33 @@
 import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
+import { useHistory} from "react-router-dom";
+import { auth, provider } from "../firebase";
+import { selectUserName, selectUserPhoto, setUserLoginDetails, setSignOutState } from "../features/user/userSlice";
 
 const Header = (props) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const userName = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
+
+    const handleAuth = () => {
+        auth.signInWithPopup(provider).then((result) => {
+            setUser(result.user);
+        }).catch((error) => {
+            alert(error.message);
+        });
+    };
+
+    const setUser = (user) => {
+        dispatch(
+            setUserLoginDetails({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+            })
+        );
+    };
+
     return (
         <Nav>
             <Logo>
@@ -32,7 +59,7 @@ const Header = (props) => {
                 <span>SERIES</span>
                 </a> 
             </NavMenu>
-            <Login></Login>
+            <Login onClick={handleAuth}>LOGIN</Login>
         </Nav>
     );
 };
@@ -138,8 +165,14 @@ const Login = styled.a`
     border: 1px solid #f9f9f9;
     border-radius: 4px;
     transition: all 0.2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
     
-    
+
 `;
 
 export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfozMygCAqsizJfj88tei4PrAnwdjWBo4",
+  authDomain: "disneyplus-clone-ea977.firebaseapp.com",
+  projectId: "disneyplus-clone-ea977",
+  storageBucket: "disneyplus-clone-ea977.firebasestorage.app",
+  messagingSenderId: "553852835032",
+  appId: "1:553852835032:web:696a8485cd666582ccf3ad"
+};
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const db = firebaseapp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
